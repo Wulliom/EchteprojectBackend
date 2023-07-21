23f05e525c916cc1cc933279f3ab2ec0d6f5175d
Wulliom <maarten.vandeelen@novi-education.nl>
2023-07-21 11:40:51 +0200

    project aangemaakt

---
+++ src/main/java/com/example/echteprojectbackend/EchteprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.echteprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchteprojectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchteprojectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/echteprojectbackend/EchteprojectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echteprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchteprojectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
